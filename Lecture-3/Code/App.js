08df56e667a2416ef12a8a4f30b71a5333eb4543
Sarika <sarikaa.memane@gmail.com>
2023-07-16 13:29:56 +0530

    Assignment on Jsx header

--- Lecture-3/Code/App.js
+++ Lecture-3/Code/App.js
@@ -31,7 +31,7 @@ const TestComponent = () => {
         {/* <HeaderComponent /> */}
       {/* {HeaderComponent()} */}
       {header}
-      {headerJsx} */}
+      {headerJsx}
     </div>
   );
 };
